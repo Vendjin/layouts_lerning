aaffb532f5bdf2546fdadb95c1475b4f2a1a35b3
Andrey_Komarovskiy <adnruxa325@gmail.com>
2022-10-27 13:11:46 +0300

    переделан gulp файл

--- pulse/_gulpfile_for_dev.js
+++ pulse/_gulpfile_for_dev.js
@@ -5,17 +5,14 @@ const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
-const htmlmin = require('gulp-htmlmin');
 
 // запуск сервера
 gulp.task('server', function () {
     browserSync.init({
         server: {
-            baseDir: "dist"
+            baseDir: "src"
         }
     });
-    // проверка изменений во всех html файлах и запуск обновления браузера
-    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 // компилируем файлы sass, (не изменения файлов, а компиляция)
@@ -36,7 +33,7 @@ gulp.task('styles', function () {
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         // скомпилированный код кладем в файл
-        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
         // после компилирования файлов обновляем браузер
         .pipe(browserSync.stream());
 });
@@ -44,40 +41,11 @@ gulp.task('styles', function () {
 // отслеживаем изменения файлов
 gulp.task('watch', function () {
     // проверяем изменения в sass файлах, и запускаем компиляцию sass файлов
-    gulp.watch("src/scss/**/*.+(scss|sass|css)", gulp.parallel("styles"))
-
-    gulp.watch("src/*.html").on("change", gulp.parallel('html'));
+    gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel("styles"))
+    // проверка изменений во всех html файлах и запуск обновления браузера
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
-
-gulp.task('html', function () {
-    return gulp.src("src/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"))
-
-});
-
-gulp.task('scripts', function () {
-    return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"))
-});
-
-gulp.task('fonts', function () {
-    return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"))
-});
-
-gulp.task('icons', function () {
-    return gulp.src("src/icons/**/*.+(svg|png)")
-        .pipe(gulp.dest("dist/icons"))
-});
-
-gulp.task('images', function () {
-    return gulp.src("src/img/**/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"))
-});
-
 
 // задача для запуска всех задач командой gulp
 // gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
-gulp.task('default', gulp.parallel('watch', 'server','styles', 'html', 'scripts', 'fonts', 'icons', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server'));
